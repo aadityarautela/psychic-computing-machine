06d9c6d52b6dca35aca04a4717bdf7639dd869a7
initdaemon <aadityarautela@protonmail.com>
2022-02-16 17:49:44 +0530

    VIDEO SCROLLS LESSGOOOOOO

--- src/organised-code/kernel/kernel.c
+++ src/organised-code/kernel/kernel.c
@@ -1,8 +1,18 @@
 #include "../drivers/screen.h"
+#include "util.h"
 
 void main() {
   cls();
-  kprint_at("XXX", 0, 24);
-  kprint_at("This text spans \n two lines", 10, 10);
-  kprint_at("RUNNING OUTAAAAAAA SPACEEEEEE\nEEEE", 45, 24);
+
+  int i = 0;
+  for (i = 0; i < 24; i++) {
+    char str[255];
+    int_to_ascii(i, str);
+    kprint_at(str, 0, i);
+  }
+
+  kprint_at("This text forces the kernel to scroll. Row 0 will disappear. ", 60,
+            24);
+  kprint("And with this text, the kernel will scroll again, and row 1 will "
+         "disappear too!");
 }
